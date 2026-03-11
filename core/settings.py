b2152015912fef3bde3187ea0b50a0f725ab74e4
bekzod-07 <bekzodsindorov0@gmail.com>
2026-03-11 11:59:22 +0500

    h

--- core/settings.py
+++ core/settings.py
@@ -1,66 +1,56 @@
 import os
 from pathlib import Path
 
-# Loyihaning asosiy papkasi
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# DIQQAT: Production'da buni o'zgartirish va maxfiy saqlash kerak!
 SECRET_KEY = 'django-insecure-n@a&7s9wgq%a6r%tjeh@jbj-i#nbkvwk@huv9j*uus0!7&968b'
 
 # SERVERDA HAR DOIM FALSE QILING
 DEBUG = False
 
-# Faqat ruxsat berilgan domenlar
-ALLOWED_HOSTS = ['api.kyotosushi.uz', 'www.api.kyotosushi.uz']
+# Domenlar
+ALLOWED_HOSTS = ['api.kyotosushi.uz', 'www.api.kyotosushi.uz', '127.0.0.1', 'localhost']
 
 # --- HTTPS VA XAVFSIZLIK SOZLAMALARI ---
-# cPanel/Ahost/Nginx kabi proxy serverlar orqali HTTPS'ni aniqlash
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 if not DEBUG:
-    # Barcha HTTP so'rovlarni HTTPS ga majburiy yo'naltirish
     SECURE_SSL_REDIRECT = True
-    # Cookie fayllarni faqat xavfsiz ulanish orqali yuborish
     SESSION_COOKIE_SECURE = True
     CSRF_COOKIE_SECURE = True
-    # HSTS - Brauzerga faqat HTTPS ishlatishni buyurish
-    SECURE_HSTS_SECONDS = 31536000  # 1 yil
+    # HSTS sozlamalari
+    SECURE_HSTS_SECONDS = 31536000
     SECURE_HSTS_INCLUDE_SUBDOMAINS = True
     SECURE_HSTS_PRELOAD = True
     # Xavfsizlik sarlavhalari
     SECURE_CONTENT_TYPE_NOSNIFF = True
     SECURE_BROWSER_XSS_FILTER = True
 else:
-    # Lokal rivojlantirish (development) uchun
     SECURE_SSL_REDIRECT = False
     SESSION_COOKIE_SECURE = False
     CSRF_COOKIE_SECURE = False
 
-# Ishonchli manbalar (CSRF xatolarini oldini olish uchun)
+# Ishonchli manbalar
 CSRF_TRUSTED_ORIGINS = [
     'https://api.kyotosushi.uz',
     'https://www.api.kyotosushi.uz'
 ]
 
-# Ilovalar
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles', # Statik fayllar uchun
     
-    # Kutubxonalar
     'rest_framework',
     'rest_framework.authtoken',
     'drf_yasg',
     
-    # Loyiha ilovalari
     'accounts',
 ]
 
-# REST Framework sozlamalari: Faqat login qilganlarga ruxsat
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
@@ -71,7 +61,6 @@ REST_FRAMEWORK = {
     ],
 }
 
-# Maxsus foydalanuvchi modeli
 AUTH_USER_MODEL = 'accounts.User'
 
 MIDDLEWARE = [
@@ -103,7 +92,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'core.wsgi.application'
 
-# Ma'lumotlar bazasi
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -111,20 +99,23 @@ DATABASES = {
     }
 }
 
-# Parol tekshiruvi
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
 ]
 
-# Til va vaqt (O'zbekiston)
 LANGUAGE_CODE = 'uz-uz'
 TIME_ZONE = 'Asia/Tashkent'
 USE_I18N = True
 USE_TZ = True 
 
-# Statik va Media fayllar
+# --- STATIK FAYLLAR SOZLAMASI ---
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+# Agar serverda 'static' papkasi ichida dizayn fayllari bo'lsa:
+STATICFILES_DIRS = [
+    # os.path.join(BASE_DIR, 'assets'), # Agar qo'shimcha papkangiz bo'lsa oching
+]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
